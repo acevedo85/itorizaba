d433433dc2af0c2a397eec29ca45c6222a1d584a
Academia <isc.acevedo@gmail.com>
2016-01-14 16:20:50 -0600

    First commit

---
+++ www/js/services.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Academia on 1/4/2016.
+ */
+
+angular.module('starter.services', []   )
+//    .service('LoginService', function ($q) {
+//    return{
+//        loginUser: function (name, pw) {
+//            var deferred = $q.defer();
+//            var promise = deferred.promise;
+//
+//            if (name == 'user' && pw == 'secret'){
+//                deferred.resolve('Bienvenido ' +name+'!');
+//            }else{
+//                deferred.reject('Credenciales erroneas.')
+//            }
+//            promise.success = function(fn){
+//                promise.then(fn);
+//                return promise;
+//            }
+//            promise.error = function(fn){
+//                promise.then(null, fn);
+//                return promise;
+//            }
+//            return promise;
+//        }
+//    }
+//});
